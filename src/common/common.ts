e422a66addf36fa9c2ad3f2f237c08c381f50604
MilanMareels <125263673+MilanMareels@users.noreply.github.com>
2024-10-25 20:44:40 +0200

    Merge into main (#4)
    * added pagination (#1)
    
    * Feat/paginatie (#3)
    
    * added pagination
    
    * added more props for return object all plants
    
    * added more props for return object all plants
    
    * Del data
    
    * fix merge
    
    * fix merge main

--- src/common/common.ts
+++ src/common/common.ts
@@ -1,44 +1,31 @@
-import { errorTypes } from '../errors/error';
+import { errorTypes } from "../errors/error";
 
-export const createResponseObject = (
-	statusCode: number,
-	body: any, // eslint-disable-line @typescript-eslint/no-explicit-any
-	res: any, // eslint-disable-line @typescript-eslint/no-explicit-any
-	// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-) => {
-	return res.status(statusCode).json(body);
+export const createResponseObject = (statusCode: number, body: any, res: any) => {
+  return res.status(statusCode).json(body);
 };
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const handleErrors = (
-	error: any, // eslint-disable-line @typescript-eslint/no-explicit-any
-	res: any, // eslint-disable-line @typescript-eslint/no-explicit-any
-	// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-) => {
-	const { message, statusCode } = checkErrorType(error);
-	// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-	return createResponseObject(statusCode!, { message: message }, res);
+export const handleErrors = (error: any, res: any) => {
+  const { message, statusCode } = checkErrorType(error);
+  return createResponseObject(statusCode!, { message: message }, res);
 };
 
 const checkErrorType = (
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	error: any,
+  error: any
 ): {
-	message: string | undefined;
-	statusCode: number | undefined;
+  message: string | undefined;
+  statusCode: number | undefined;
 } => {
-	let message, statusCode;
-	for (const errorType of errorTypes) {
-		if (error instanceof errorType) {
-			message = error.message;
-			statusCode = error.statusCode;
-			break;
-		}
-	}
-	return { message, statusCode };
+  let message, statusCode;
+  for (const errorType of errorTypes) {
+    if (error instanceof errorType) {
+      message = error.message;
+      statusCode = error.statusCode;
+      break;
+    }
+  }
+  return { message, statusCode };
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const isNullOrUndefined = (obj: any): boolean => {
-	return obj === null || obj === undefined;
+  return obj === null || obj === undefined;
 };
